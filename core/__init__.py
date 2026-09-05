1623b0c245a06bc9825f18a90eb84bd006d5e86e
Christopher Holmes <c1490b@gmail.com>
2026-09-04 18:27:38 -0700

    Add core/__init__.py - Core module initialization

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+"""AI Line Core Module"""
+
+from .ailine import AILine
+from .device_manager import DeviceManager
+from .inference_engine import InferenceEngine
+
+__all__ = ["AILine", "DeviceManager", "InferenceEngine"]
